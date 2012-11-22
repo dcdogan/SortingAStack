ff4e16440144c861ee7bae35400732f7ef75553f
Dogan Can <ddogan1@binghamton.edu>
2012-11-22 16:35:55 +0200

    Initial Commit

---
+++ SortingAStack/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SortingAStack
+//
+//  Created by DoganCan on 11/22/12.
+//  Copyright (c) 2012 Dogan Can Dogan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
